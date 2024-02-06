a0eb7fd56b78567ea8e9a756d33faeac20a9c089
Albert <witsalbert@gmail.com>
2024-02-06 09:44:21 +0800

    init project

---
+++ src/main/java/com/example/orderitemproject_20240206/OrderItemProject20240206Application.java
@@ -0,0 +1,13 @@
+package com.example.orderitemproject_20240206;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderItemProject20240206Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderItemProject20240206Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/orderitemproject_20240206/OrderItemProject20240206ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.orderitemproject_20240206;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderItemProject20240206ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
